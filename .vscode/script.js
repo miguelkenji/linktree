f88b47dba42ef9e966d5f2ad3507dde703c58772
Miguel Kenji <miguelkenji@MacBook-Air-de-Miguel.local>
2023-10-13 20:43:50 +0200

    :tadda: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+  console.log({ img })
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    console.log("passo if")
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    console.log("passo else")
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
